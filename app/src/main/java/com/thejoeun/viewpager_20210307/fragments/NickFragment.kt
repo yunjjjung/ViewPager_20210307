917950e62713bb52681092d6af2510f54c4e8487
최윤정 <jjjung1003@naver.com>
2021-03-07 12:35:26 +0900

    닉네임 프레그먼트 추가

---
+++ app/src/main/java/com/thejoeun/viewpager_20210307/fragments/NickFragment.kt
@@ -0,0 +1,25 @@
+package com.thejoeun.viewpager_20210307.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.thejoeun.viewpager_20210307.R
+
+class NickFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_nick, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+
+    }
+}
